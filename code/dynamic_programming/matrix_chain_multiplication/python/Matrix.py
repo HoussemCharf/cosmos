3cc146376db91b5f20d036d08f1c61b7d616cf36
HoussemCharf <houssem.charf@gmail.com>
2017-10-14 16:21:02 +0100

    Python solution for matrix chain multiplication issue #1468

---
+++ code/dynamic_programming/matrix_chain_multiplication/python/Matrix.py
@@ -0,0 +1,24 @@
+import sys
+def MatrixChainOrder(x, y):
+    matrice = [[0 for x in range(y)] for x in range(y)]
+
+    for i in range(1, y):
+        matrice[i][i] = 0
+    for z in range(2, y):
+        for i in range(1, y-z+1):
+            j = i+z-1
+            matrice[i][j] = sys.maxint
+            for k in range(i, j):
+                q = matrice[i][k] + matrice[k+1][j] + x[i-1]*x[k]*x[j]
+                if q < matrice[i][j]:
+                    matrice[i][j] = q
+ 
+    return matrice[1][y-1]
+array = list()
+num = raw_input("Enter how many elements you want:")
+print 'Enter numbers in array: '
+for i in range(int(num)):
+    n = raw_input("num :")
+    array.append(int(n))
+length = len(array)
+print("result =" +str(MatrixChainOrder(array, length)))
